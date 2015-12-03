7856852dfe968c7131228a7cf70cbfebe980f209
David Baliles <david@baliles.com>
2015-12-03 07:14:06 -0500

    DB

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php 
+
+$host = "ec2-107-20-223-116.compute-1.amazonaws.com";
+$port = "5432";
+$dbname = "d31r0cgdueumjf";
+$user = "uqphasfcijtgrg";
+$password = "lCwnoVxA3zJgIhomgB6mq9Xsqt";
+$pg_options = "--client_encoding=UTF8";
+
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ssl=true sslfactory=org.postgresql.ssl.NonValidatingFactory options='{$pg_options}'";
+$dbconn = pg_connect($connection_string);
+
+
+if($dbconn){
+    echo "Connected to ". pg_host($dbconn); 
+}else{
+    echo "Error in connecting to database.";
+}
+
+echo "<br />";
+?>
