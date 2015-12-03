59d6e5698ae9c6370fab9ba2b9112796a0855461
David Baliles <david@baliles.com>
2015-12-03 07:35:55 -0500

    New DB

--- db.php
+++ db.php
@@ -1,35 +1,116 @@
-<?php 
 
-$host = "ec2-107-20-223-116.compute-1.amazonaws.com";
-$port = "5432";
-$dbname = "d31r0cgdueumjf";
-$user = "uqphasfcijtgrg";
+
+<?php
+$database = "d31r0cgdueumjf";
+$username = "uqphasfcijtgrg";
 $password = "lCwnoVxA3zJgIhomgB6mq9Xsqt";
-$pg_options = "--client_encoding=UTF8";
-
-$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} options='{$pg_options}'";
-$dbconn = pg_connect($connection_string);
-
-
-if($dbconn){
-    echo "Connected to ". pg_host($dbconn); 
-}else{
-    echo "Error in connecting to database.";
+$db = new PDO("mysql:host=ec2-107-20-223-116.compute-1.amazonaws.com;dbname=".$database , $username, $password);
+$statement = $db->prepare("SELECT * FROM questions");
+$statement->execute( array(18) );
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+$personsTable = "";
+if(count($rows) > 0)
+{
+    $table = new View_Generator_Table( array("question","question_type","id") );
+    foreach($rows as $row)
+    {
+        $table->addCell( $row["question"] );
+        $table->addCell( $row["question_type"] );
+        $table->addCell( $row["id"] );
+    }
+    $personsTable = $table->generate();
 }
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>An example with a table</title>
+</head>
+<body>
+RANDOM
+    <br />
+    <?=$personsTable?>
+</body>
+</html>
+<?php
+    /**
+     * A very simple view component generator for tables 
+     */
+    class View_Generator_Table
+    {
+        private $headers = array();
+        private $cells = array();
+        /**
+         * Array of the table headers
+         * @param array of strings $headers
+         */
+        public function __construct($headers)
+        {
+            $this->headers = $headers;
+        }
+        /**
+         * Adds a cell to the table
+         * @param string $cell
+         */
+        public function addCell($cell = "")
+        {
+            $this->cells[] = $cell;
+        }
+        /**
+         * Generates and returns the table html in a  string
+         * @return string
+         */
+        public function generate()
+        {
+            $re = "";
+            $columns = count($this->headers);
+            if($columns  > 0)
+            {
+                $re .= "<table><thead><tr>";
+                // Adding the headers
+                foreach($this->headers as $header)
+                {
+                    $re .= "<th>".$header."</th>";
+                }
+                $re .= "</thead></tr>";
+                $totalCells = count($this->cells);
+                if($totalCells > 0)
+                {
+                    // Adding the data cells 
+                    $re .= "<tbody>";
+                    for($i=0;  $i < $totalCells; $i++)
+                    {
+                        $currentColumn = $i % $columns;
+                        if($currentColumn == 0)
+                        {
+                            $re .= "<tr>";
+                        }
+                        $re .= "<td>".$this->cells[$i]."</td>";
+                        if($currentColumn == $columns - 1)
+                        {
+                            $re .= "</tr>";
+                        }
+                    }
+                    // If there the number of the cells don't much the number of the 
+                    // columns then we add some empty cells 
+                    if($currentColumn !== $columns - 1)
+                    {
+                        for($i = $currentColumn ;  $i < $columns - 1; $i++)
+                        {
+                            $re .= "<td></td>";
+                        }
+                        $re .= "</tr>";
+                    }
+                    $re .= "</tbody>";
+                }
+                $re .= "</table>";
+            }
+            return $re;
+        }
+    }
+?>
 
-echo "<br />";
 
-require_once 'index.php';
-
-$result = pg_query($dbconn, "SELECT * FROM users");
-if (!$result) {
-    echo "An error occurred.\n";
-    exit;
-}
-
-$arr = pg_fetch_all($result);
-
-echo "<pre>";
-print_r($arr);
-echo "</pre>";
 ?>
